c8bf4c08e3c266ecad1b72434e343372b8a0e613
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-11-16 13:41:50 +0100

    updated

--- dashboard/includes/footer.php
+++
@@ -1,7 +0,0 @@
-  <footer class="main-footer">
-      <strong>&copy; De-Guide Light School <?php echo date("Y"); ?></strong>
-      <div class="float-right d-none d-sm-inline-block">
-          <strong>Developed by: <a href="https://doteightplus.com" target="_blank"><span
-                      style="color: red; ">DotEightPlus</span></a></strong>
-      </div>
-  </footer>
